50ff357efa262471dabce869ff1afa1f728692c0
sigtot <sigurdtotland@gmail.com>
2021-02-03 14:50:27 +0100

    Add ros

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <ros/ros.h>
+
 using namespace std;
-int main() {
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "orb_test");
+    ros::NodeHandle nh;
     cout << "Hello world" << endl;
     return 0;
 }
