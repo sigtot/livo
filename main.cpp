18c097f32395a7426ca6834eea788660aa15e9ba
sigtot <sigurdtotland@gmail.com>
2021-02-03 16:12:16 +0100

    Set up image subscriber

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <ros/ros.h>
+#include <sensor_msgs/Image.h>
 
 using namespace std;
+
+void imageCallback(const sensor_msgs::Image::ConstPtr& img) {
+    ROS_INFO("Recvd img\n");
+}
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "orb_test");
     ros::NodeHandle nh;
-    cout << "Hello world" << endl;
+
+    auto sub = nh.subscribe("/camera/image_mono", 1000, imageCallback);
+
+    ros::spin();
     return 0;
 }
