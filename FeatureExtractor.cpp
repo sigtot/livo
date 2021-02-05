962b3f3bc82d8a25be7d240bfcb635af9e40c644
sigtot <sigurdtotland@gmail.com>
2021-02-05 17:23:16 +0100

    Add Landmark, Frame and KeyPointObservation structs

--- FeatureExtractor.cpp
+++ FeatureExtractor.cpp
@@ -1,20 +1,14 @@
-#include <vector>
 #include "FeatureExtractor.h"
 
 #include <cv_bridge/cv_bridge.h>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/core/core.hpp>
-
 
 FeatureExtractor::FeatureExtractor(const ros::Publisher &pub) : pub(pub) {}
-
-using namespace std;
-using namespace cv;
 
 const int MAX_FEATURES = 500;
 
 void FeatureExtractor::imageCallback(const sensor_msgs::Image::ConstPtr &msg) {
-    auto cvPtr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_8UC1); // Makes copy. We can also share to increase performance
+    auto cvPtr = cv_bridge::toCvCopy(msg,
+                                     sensor_msgs::image_encodings::TYPE_8UC1); // Makes copy. We can also share to increase performance
 
     Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE_HAMMING);
@@ -26,6 +20,35 @@ void FeatureExtractor::imageCallback(const sensor_msgs::Image::ConstPtr &msg) {
 
     cv_bridge::CvImage outImg(msg->header, sensor_msgs::image_encodings::TYPE_8UC3);
     drawKeypoints(cvPtr->image, keyPoints, outImg.image);
+
+    if (!frames.empty()) {
+        auto prevKeyPoints = frames.back().getKeyPoints();
+        cout << "type" << descriptors.type() << endl;
+        auto prevDescriptors = frames.back().getDescriptors();
+        int i = 0;
+        for (const auto &kp : prevKeyPoints) {
+            cout << "(" << kp.pt.x << "," << kp.pt.y << ") " << endl;
+            cout << prevDescriptors.row(i) << endl;
+            cout << descriptors.row(i) << endl;
+            i++;
+        }
+    }
+
+    Frame newFrame;
+    int i = 0;
+    for (auto &kp : keyPoints) {
+        Landmark landmark; // ONLY DO THIS ON NEW LANDMARK INITIALIZATION, NOT EVERY TIME
+        cout << i << " " << descriptors.row(i).clone() << endl;
+        KeyPointObservation observation{
+                .keyPoint = kp,
+                .descriptor = descriptors.row(i).clone(),
+                .landmark = &landmark,
+                .frame = &newFrame};
+        landmark.keyPointObservations.push_back(&observation);
+        newFrame.keyPointObservations.push_back(observation);
+        i++;
+    }
+    frames.push_back(newFrame);
 
     pub.publish(outImg.toImageMsg());
 }
--- FeatureExtractor.h
+++ FeatureExtractor.h
@@ -1,16 +1,58 @@
 #ifndef ORB_TEST_FEATUREEXTRACTOR_H
 #define ORB_TEST_FEATUREEXTRACTOR_H
 
+#include <vector>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+using namespace cv;
+
+struct KeyPointObservation;
+struct Landmark;
+struct Frame;
+
+struct KeyPointObservation {
+    KeyPoint keyPoint;
+    Mat descriptor;
+    Landmark *landmark;
+    Frame *frame;
+};
+
+struct Landmark {
+    vector<KeyPointObservation*> keyPointObservations;
+};
+
+struct Frame {
+    vector<KeyPoint> getKeyPoints() const {
+        vector<KeyPoint> keyPoints; // TODO reserve space up front to avoid resizes
+        transform(keyPointObservations.begin(), keyPointObservations.end(), back_inserter(keyPoints),
+                  [](const KeyPointObservation& o) -> KeyPoint { return o.keyPoint; });
+        return keyPoints;
+    }
+
+    Mat getDescriptors() const {
+        Mat descriptors;
+        for (const auto& obs : keyPointObservations) {
+            descriptors.push_back(obs.descriptor);
+        }
+        return descriptors;
+    }
+
+    vector<KeyPointObservation> keyPointObservations;
+    int seq{};
+};
 
 class FeatureExtractor {
 private:
     ros::Publisher pub;
+    vector<Frame> frames;
 public:
-    FeatureExtractor(const ros::Publisher &pub);
+    explicit FeatureExtractor(const ros::Publisher &pub);
 
-    void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
+    void imageCallback(const sensor_msgs::Image::ConstPtr &msg);
 };
 
 
