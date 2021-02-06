242e1a3d851f7acc8c82e3108c333ebea4253647
sigtot <sigurdtotland@gmail.com>
2021-02-06 18:47:49 +0100

    Implement (very slow) gridded orb extraction

--- FeatureExtractor.cpp
+++ FeatureExtractor.cpp
@@ -6,9 +6,19 @@ FeatureExtractor::FeatureExtractor(const ros::Publisher &pub) : pub(pub) {}
 
 const int MAX_FEATURES = 500;
 
+// The current grid implementation slows down the code a lot, so I've disabled it by setting GRID_SIZE=1
+const int GRID_SIZE = 1;
+
 void FeatureExtractor::imageCallback(const sensor_msgs::Image::ConstPtr &msg) {
     auto cvPtr = cv_bridge::toCvCopy(msg,
                                      sensor_msgs::image_encodings::TYPE_8UC1); // Makes copy. We can also share to increase performance
+
+    int imgHeight = msg->height;
+    int imgWidth = msg->width;
+
+    int gridBlockHeightPx = imgHeight / GRID_SIZE;
+    int gridBlockWidthPx = imgWidth / GRID_SIZE;
+
 
     Ptr<Feature2D> orb = ORB::create(MAX_FEATURES);
     Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::BRUTEFORCE_HAMMING);
@@ -16,7 +26,20 @@ void FeatureExtractor::imageCallback(const sensor_msgs::Image::ConstPtr &msg) {
     vector<KeyPoint> keyPoints;
     Mat descriptors;
 
-    orb->detectAndCompute(cvPtr->image, Mat(), keyPoints, descriptors);
+    for (int i = 0; i < GRID_SIZE; ++i) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            vector<KeyPoint> newKeyPoints;
+            Mat newDescriptors;
+
+            Mat mask;
+            mask = Mat::zeros(cvPtr->image.rows, cvPtr->image.cols, CV_8U);
+            mask(Rect(i * gridBlockWidthPx, j * gridBlockHeightPx, gridBlockWidthPx, gridBlockHeightPx)) = 1;
+
+            orb->detectAndCompute(cvPtr->image, mask, newKeyPoints, newDescriptors);
+            keyPoints.insert(keyPoints.end(), newKeyPoints.begin(), newKeyPoints.end());
+            descriptors.push_back(newDescriptors);
+        }
+    }
 
     if (!frames.empty()) {
         auto prevFrame = frames.back();
