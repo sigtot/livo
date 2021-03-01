08582940ef636c9b6cd04bdfa6d46cfec68caa6a
sigtot <sigurdtotland@gmail.com>
2021-03-01 21:49:49 +0100

    Move header files into /include/livo

--- style-check.sh
+++ style-check.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # TODO: Does not recursively search directories
-cat **/*.{cpp,h} | clang-format-10 --dry-run -Werror
+cat src/*.cpp include/livo/*.h | clang-format-10 --dry-run -Werror
