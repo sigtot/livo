bd9cffc40c090c2c7c26df7dfc7ee84fbabeac6b
sigtot <sigurdtotland@gmail.com>
2021-06-17 21:37:14 +0200

    Clean TF2betweenTransformProvider::GetBetweenTransform

--- src/tf2_between_transform_provider.cpp
+++ src/tf2_between_transform_provider.cpp
@@ -36,14 +36,15 @@ boost::optional<gtsam::Pose3> TF2BetweenTransformProvider::GetBetweenTransform(d
     auto tf_1 = tf_buffer.lookupTransform(world_frame_, sensor_frame_, ros::Time(timestamp1)).transform;
     auto tf_2 = tf_buffer.lookupTransform(world_frame_, sensor_frame_, ros::Time(timestamp2)).transform;
 
-    gtsam::Pose3 w_T_l1(gtsam::Rot3::Quaternion(tf_1.rotation.w, tf_1.rotation.x, tf_1.rotation.y, tf_1.rotation.z),
+    gtsam::Pose3 loam_T_l1(gtsam::Rot3::Quaternion(tf_1.rotation.w, tf_1.rotation.x, tf_1.rotation.y, tf_1.rotation.z),
                         gtsam::Point3(tf_1.translation.x, tf_1.translation.y, tf_1.translation.z));
-    gtsam::Pose3 w_T_l2(gtsam::Rot3::Quaternion(tf_2.rotation.w, tf_2.rotation.x, tf_2.rotation.y, tf_2.rotation.z),
+    gtsam::Pose3 loam_T_l2(gtsam::Rot3::Quaternion(tf_2.rotation.w, tf_2.rotation.x, tf_2.rotation.y, tf_2.rotation.z),
                         gtsam::Point3(tf_2.translation.x, tf_2.translation.y, tf_2.translation.z));
-    auto w_T_b1 = w_T_l1 * body_p_sensor_->inverse();
-    auto w_T_b2 = w_T_l2 * body_p_sensor_->inverse();
-    auto between = w_T_b1.between(w_T_b2);
-    return between;
+
+    auto l1_T_l2 = loam_T_l1.inverse() * loam_T_l2;
+    auto b1_T_b2 = *body_p_sensor_ * l1_T_l2 * body_p_sensor_->inverse();
+
+    return b1_T_b2;
   }
   catch (tf2::TransformException& ex)
   {
